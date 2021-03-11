2facd143d5e08e7d4d5bc471efe075af5e1d29b2
NSokolov114 <sokoloff114@gmail.com>
2021-03-11 20:54:49 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length === 0) return 0;
+  return array.sort(function(a, b) {  return a - b;})[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length === 0) return 0;
+  return array.sort(function(a, b) {  return a - b; }).reverse()[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length === 0) return 0;
+  let sum = 0;
+  array.forEach(function(value) {
+    sum += value;
+  })
+  return sum/array.length;
 }
